091103b6c05bc27efefc907ec160f047b2a2a007
JOHN DIEGO <johnoliveirasilva9@gmail.com>
2020-10-09 23:52:39 -0300

    Create controler etapa

--- app/controllers/etapa_controller.rb
+++ app/controllers/etapa_controller.rb
@@ -1,4 +1,25 @@
 class EtapaController < ApplicationController
+  def create
+    @etapa = @task.etapas.create(etapa_params)
+  end
 
+  def destroy
+    @etapa = @task.etapas.find(params[:id])
+    if @etapa.destroy
+      flash[:sucess] = "Task etapa was deleted."
+    else
+      flash[:error] = "Task etapa could not be deleted"
+    end  
+    redirect_to @board
+  end
+  
+  private
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def etapa_params
+    params[:etapa].permit(:title)
+  end
  
 end
