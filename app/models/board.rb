f19654ce15dcde4405f70ccfbb1732b495805f40
John Diego <johnoliveirasilva9@gmail.com>
2020-10-12 16:32:47 -0300

    Validation Board

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,3 +1,5 @@
 class Board < ApplicationRecord
   has_many :tasks
+  validates :title, presence: { message: "O campo de texto não pode ficar em branco"} 
+  validates :title, uniqueness: { case_sensitive: false, message: "Não pode haver mais de um board com o mesmo nome"} 
 end
