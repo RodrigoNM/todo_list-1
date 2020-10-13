8f8dfcf102bdef1ba72e5d5855e346605d890e2a
JOHN DIEGO <johnoliveirasilva9@gmail.com>
2020-10-13 14:03:11 -0300

    Add validades model board

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,5 +1,6 @@
 class Board < ApplicationRecord
   has_many :tasks
-  validates :title, presence: { message: "O campo de texto não pode ficar em branco"} 
-  validates :title, uniqueness: { case_sensitive: false, message: "Não pode haver mais de um board com o mesmo nome"} 
+  validates_associated :tasks
+  validates :title, presence: { message: "can't be blank"} 
+  validates :title, uniqueness: { case_sensitive: false, message: "are uniquenes"} 
 end
